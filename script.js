784f4cf6809a11ca4af089dd6cad98db2795f967
phanduc <jase.phan@mail.utoronto.ca>
2025-02-16 21:32:52 -0500

    some skeleton code

---
+++ script.js
@@ -0,0 +1,13 @@
+/*const home = document.querySelector(".home"),
+    aboutMe = document.querySelector(".about-me"), 
+    proj = document.querySelector(".my-project"), 
+    cv = document.querySelector(".my-cv"), 
+    gym = document.querySelector(".gym"), 
+    contact = document.querySelector(".contact");*/
+
+// home.addEventListener("click", () => location.replace("youtube.com"))
+
+function openLink(event, s) {
+    location.replace("https://youtube.com"); 
+}
+    
